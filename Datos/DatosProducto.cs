a20bf186bd55d006a96781be3635dd4fba502e20
nobodynuf <darkgod.of.darkness.corporation@gmail.com>
2017-10-16 19:49:36 -0300

    Agregados accesadores de ADO de Tabla Producto

---
+++ Datos/DatosProducto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class DatosProducto
+    {
+        // CRD
+        TiendaElectronicaEntities1 te;
+
+        public DatosProducto()
+        {
+            te = new TiendaElectronicaEntities1();
+        }
+
+        public bool Crear(Producto producto)
+        {
+            var lista = (from a in te.Producto select a).ToList();
+
+            foreach (Producto item in lista)
+            {
+                if (item.codigo == producto.codigo)
+                {
+                    return false;
+                }
+            }
+            te.Producto.Add(producto);
+            te.SaveChanges();
+            return true;
+        }
+
+        public List<Producto> Listar() {
+            return (from a in te.Producto select a).ToList();
+        }
+
+        public bool EliminarPorCodigo(int codigo)
+        {
+            var lista = (from a in te.Producto select a).ToList();
+
+            foreach (Producto item in lista)
+            {
+                if (item.codigo == codigo)
+                {
+                    te.Producto.Remove(item);
+                    te.SaveChanges();
+                    return true;
+                }
+            }
+            return false;
+        }
+        public List<Producto> Listar(int precioMenor, int precioMayor)
+        {
+            return (from a in te.Producto
+                    where a.precio > precioMenor &
+                        a.precio < precioMayor
+                    select a).ToList();
+        }
+    }
+}
---
+++ Vista/WebEliminarCodigo.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/TiendaElectronica.Master" AutoEventWireup="true" CodeBehind="WebEliminarCodigo.aspx.cs" Inherits="Vista.WebEliminarCodigo" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
